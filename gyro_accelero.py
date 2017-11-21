6023bd96cc0366fd9100e7f66f81f0cdb014f4f9
Kavish Bhatia <kavishbhatia@live.in>
2017-11-21 16:36:04 +0530

    Update gyro_accelero.py

--- gyro_accelero.py
+++ gyro_accelero.py
@@ -8,10 +8,20 @@ GPIO.setmode(GPIO.BCM)
 GPIO.cleanup()
 
 while True:
+    #making object of class MPU6050
     mpu = MPU6050.MPU6050()
+    
+    #calling initializing function
     mpu.init()
+
+    #get gyro data
     gyro = mpu.gyro_data()
+
+    #get accelerometer data
     accel = mpu.accelerometer_data()
+
+    #print acquired data
     print "gyro(x, y, z) - ", gyro
     print "accelerometer(x, y, z) - ", accel
+
     time.sleep(1)
